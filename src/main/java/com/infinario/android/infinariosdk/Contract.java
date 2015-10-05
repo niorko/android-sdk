bcad05ff1aadef571be827d4210e9e1d91cb1b38
Roland Rogansky <roland.rogansky@infinario.com>
2015-10-05 13:31:32 +0200

    Update version numbers to 1.1.3

--- src/main/java/com/infinario/android/infinariosdk/Contract.java
+++ src/main/java/com/infinario/android/infinariosdk/Contract.java
@@ -16,7 +16,7 @@ public class Contract {
      * SDK
      */
     public static final String SDK = "AndroidSDK";
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.3";
     public static final String OS = "Android";
 
     /**
