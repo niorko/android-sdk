63b0f960b5aeec0464ffece0d31bb6bd75b99803
Filip Bajaník <filip.bajanik@kiwi.com>
2017-04-13 11:53:46 +0200

    - Removed non-existing dependencies

--- src/main/java/com/infinario/android/infinariosdk/Infinario.java
+++ src/main/java/com/infinario/android/infinariosdk/Infinario.java
@@ -26,16 +26,6 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
 
